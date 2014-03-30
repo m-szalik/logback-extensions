3eefc4cd92417f9461a30aab51931b5a72086762
Marek Szalik <szalik@jsoftware.org>
2014-03-31 01:17:13 +0200

    XMMP Appender

---
+++ src/test/java/org/jsoftware/logback/XmmpAppenderTest.java
@@ -0,0 +1,34 @@
+package org.jsoftware.logback;
+
+import org.junit.After;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class XmmpAppenderTest {
+
+	@Test 
+	public void testSimpleMessage() {
+		Logger logger = LoggerFactory.getLogger(getClass());
+		logger.info("Test message 1");
+		logger.warn("Test message 2");
+	}
+
+	@Test 
+	public void testMessageWithException() {
+		Logger logger = LoggerFactory.getLogger(getClass());
+		try {
+			throw new Exception("Exception message.");
+		} catch (Exception e) {
+			logger.info("Exception text.", e);
+		}
+	}
+	
+	
+	
+	@After
+	public void waitFor() throws InterruptedException {
+		Thread.sleep(1000);
+	}
+}
